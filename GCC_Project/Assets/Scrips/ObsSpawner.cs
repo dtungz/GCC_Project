b454f4ab8129525d1362b192d350721e8acdc795
zKay <ductung.ptit@gmail.com>
2025-03-29 00:41:18 +0700

    thêm danh sách perfabs

---
+++ GCC_Project/Assets/Scrips/ObsSpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class ObsSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] obsPrefabs;
+    public float obsSpawnTime = 3f;
+    private float countDown;
+
+    private void Update()
+    {
+        SpawnLoop();
+    }
+    private void SpawnLoop()
+    {
+        countDown += Time.deltaTime;
+        if(countDown >= obsSpawnTime)
+        {
+            Spawn();
+            countDown = 0;
+        }
+    }
+    private void Spawn()
+    {
+        GameObject obsSpawn = obsPrefabs[Random.Range(0,obsPrefabs.Length)];
+        GameObject posSpawn = Instantiate(obsSpawn,transform.position,Quaternion.identity);
+    }
+}
