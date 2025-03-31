7247fd9205eabf3b60239bbfc3b1272eca7cf6a6
Serinary <ductoan110906@gmail.com>
2025-04-01 00:03:30 +0700

    ném file prefabs ra ngoài

--- GCC_Project/Assets/Scripts/ObstacleTrigger.cs
+++ GCC_Project/Assets/Scripts/ObstacleTrigger.cs
@@ -24,6 +24,7 @@ public class ObstacleTrigger : MonoBehaviour
             {
                 pm.canBeTrigger = false;
                 StartCoroutine(CollsionReset());
+                pm.isShielded = false;
                 return;
             }
         }
