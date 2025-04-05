53481784b15c6187c5b015feaf64202ca7876204
Serinary <ductoan110906@gmail.com>
2025-04-05 23:46:40 +0700

    thêm một scene UI main menu

---
+++ GCC_Project/Assets/Scripts/Game/ButtonUnderCursor.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class ButtonUnderCursor : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Vector3 normalSize = new Vector3(1f, 1f, 1f);
+    public Vector3 underCursorSize = new Vector3(0.8f, 0.8f, 1f);
+    private RectTransform rt;
+
+    void Start()
+    {
+        rt = GetComponent<RectTransform>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        rt.localScale = underCursorSize;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        rt.localScale = normalSize;
+    }
+}
