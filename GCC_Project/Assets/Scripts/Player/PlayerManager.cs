66de000f6d0287a017ddaf488c7e27db087c5ddc
Serinary <ductoan110906@gmail.com>
2025-04-13 18:12:36 +0700

    sửa Move + fix lại bug Shield

--- GCC_Project/Assets/Scripts/Player/PlayerManager.cs
+++ GCC_Project/Assets/Scripts/Player/PlayerManager.cs
@@ -1,57 +1,18 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
     public float force = 15f;
-    [SerializeField] Rigidbody2D rb;
+    [SerializeField] private Rigidbody2D rb;
+
     public bool isShielded;
     public bool isInvincible;
     public bool isShrinked;
-    public bool canBeTrigger;
+
     private Vector3 originalScale;
-
-    void Move()
-    {
-        if(Input.GetButton("Jump"))
-        {
-            rb.AddForce(Vector2.up * force, ForceMode2D.Force);
-        }
-    }
-    IEnumerator SizeReset()
-    {
-        yield return new WaitForSeconds(8f);
-        transform.localScale = originalScale;
-        isShrinked = false;
-    }
-
-    IEnumerator InvincibleReset()
-    {
-        yield return new WaitForSeconds(8f);
-        isInvincible = false;
-    }
-    private void Invincible()
-    {
-        if (isInvincible)
-        {
-            if (InvincibleReset() != null) StopCoroutine(InvincibleReset());
-            StartCoroutine(InvincibleReset());
-        }
-        else return;
-    }
-    private void Shrink()
-    {
-        if (isShrinked && transform.localScale.x > originalScale.x * 0.5f)
-        {
-            transform.localScale = new Vector3(
-                originalScale.x * 0.5f, 
-                originalScale.y * 0.5f,
-                originalScale.z
-            );
-            StartCoroutine(SizeReset());
-        }
-    }
+    private Coroutine invincibleCoroutine;
+    private Coroutine shrinkCoroutine;
 
     void Start()
     {
@@ -59,15 +20,52 @@ public class PlayerManager : MonoBehaviour
         isShielded = false;
         isInvincible = false;
         isShrinked = false;
-        canBeTrigger = true;
         originalScale = transform.localScale;
     }
 
     void Update()
     {
         Move();
-        if (isShrinked) Shrink();
-        Invincible();
+
+        if (isShrinked && shrinkCoroutine == null)
+        {
+            shrinkCoroutine = StartCoroutine(ShrinkReset());
+        }
+
+        if (isInvincible && invincibleCoroutine == null)
+        {
+            invincibleCoroutine = StartCoroutine(InvincibleReset());
+        }
+    }
+
+    private void Move()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            rb.gravityScale = -1;
+        }
+        else rb.gravityScale = 1;   
+    }
+
+    private IEnumerator ShrinkReset()
+    {
+        transform.localScale = new Vector3(
+            originalScale.x * 0.5f,
+            originalScale.y * 0.5f,
+            originalScale.z
+        );
+
+        yield return new WaitForSeconds(8f);
+
+        transform.localScale = originalScale;
+        isShrinked = false;
+        shrinkCoroutine = null;
+    }
+
+    private IEnumerator InvincibleReset()
+    {
+        yield return new WaitForSeconds(8f);
+        isInvincible = false;
+        invincibleCoroutine = null;
     }
 }
-
